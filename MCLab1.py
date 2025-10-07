0231d05adf2ab60e7f0f9a578fb01deb750aeb82
JBCinco <jcinco@ilstu.edu>
2025-10-07 21:23:44 +0100

    Add files via upload

---
+++ MCLab1.py
@@ -0,0 +1,13 @@
+
+from mcpi_addons.minecraft import Minecraft
+mc = Minecraft.create()
+
+import time
+
+
+count = 0
+while count < 30:
+    pos = mc.player.getPos()
+    mc.setBlock(pos.x, pos.y -1, pos.z, 12)
+    count += 1
+    time.sleep(1)
